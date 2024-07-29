d1c6fd69dde6bf60df3805fb4d603fc557b1cd72
bebomusa <lj6976294@mail.com>
2024-07-29 04:25:19 -0400

    include sql

---
+++ graffiti.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `graffiti` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `creator_id` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `coords` LONGTEXT NOT NULL,
+  `dimension` INT DEFAULT 0,
+  `text` LONGTEXT NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
