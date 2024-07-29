b5e26cde9fa4df52db82dbe781647fdab1edbfc9
bebomusa <lj6976294@mail.com>
2024-07-29 04:25:43 -0400

    fix(web): build location

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
-    outDir: '../../dist/web',
+    outDir: 'dist',
     emptyOutDir: true,
   },
   resolve: {
