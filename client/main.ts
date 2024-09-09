840ca38820dbf16ec9433ca58eafb9534d60a0d7
arlofonseca <lj6976294@mail.com>
2024-09-08 22:18:37 -0400

    chore: remove prettier
    Don't know why prettier just destroys the code. I can change the tabWidth and try to adjust it but it just doesn't ever work so here we are.

--- client/main.ts
+++ client/main.ts
@@ -20,66 +20,54 @@ const drawGraffiti: Record<number, boolean> = {};
 const points: Record<number, Point> = {};
 let playerBucket = 0;
 
-netEvent(
-  'fivem-graffiti:client:createGraffitiTag',
-  (
-    id: number,
-    creator_id: string,
-    coords: [number, number, number],
-    dimension: number,
-    text: string,
-    font: number,
-    size: number,
-    hex: string
-  ) => {
-    const graffiti: Graffiti = {
-      id: id,
-      creator_id: creator_id,
-      coords: Cfx.Vector3.fromArray(coords),
-      dimension: dimension,
-      text: text,
-      font: font,
-      size: size,
-      hex: hex,
-      displayed: false,
-    };
+netEvent('fivem-graffiti:client:createGraffitiTag', (id: number, creator_id: string, coords: [number, number, number], dimension: number, text: string, font: number, size: number, hex: string) => {
+  const graffiti: Graffiti = {
+    id: id,
+    creator_id: creator_id,
+    coords: Cfx.Vector3.fromArray(coords),
+    dimension: dimension,
+    text: text,
+    font: font,
+    size: size,
+    hex: hex,
+    displayed: false,
+  };
 
-    createdGraffiti[id] = graffiti;
-    drawGraffiti[id] = false;
+  createdGraffiti[id] = graffiti;
+  drawGraffiti[id] = false;
 
-    startSpray(graffiti.coords, text, font, size, hex);
+  startSpray(graffiti.coords, text, font, size, hex);
 
-    points[id] = new Point({
-      coords: coords,
-      distance: config.graffiti_distance,
-      nearby: async (): Promise<void> => {
-        const plyCoords: Cfx.Vector3 = Cfx.Game.PlayerPed.Position;
-        const distance: number = plyCoords.distance(graffiti.coords);
-        if (playerBucket === graffiti.dimension && distance <= config.graffiti_distance) {
-          drawGraffiti[id] = true;
+  points[id] = new Point({
+    coords: coords,
+    distance: config.graffiti_distance,
+    nearby: async (): Promise<void> => {
+      const plyCoords: Cfx.Vector3 = Cfx.Game.PlayerPed.Position;
+      const distance: number = plyCoords.distance(graffiti.coords);
+      if (playerBucket === graffiti.dimension && distance <= config.graffiti_distance) {
+        drawGraffiti[id] = true;
 
-          if (distance <= 1.5 && !graffiti.displayed) {
-            // todo: display graffiti
-            graffiti.displayed = true;
-          } else if (distance > 1.5) {
-            graffiti.displayed = false;
-          }
-        } else {
+        if (distance <= 1.5 && !graffiti.displayed) {
+          // todo: display graffiti
+          graffiti.displayed = true;
+        } else if (distance > 1.5) {
           graffiti.displayed = false;
-          drawGraffiti[id] = false;
         }
-      },
-      onEnter: () => {
-        console.log(`Entered range of point ${id}`);
-      },
-      onExit: () => {
-        console.log(`Left range of point ${id}`);
+      } else {
         graffiti.displayed = false;
         drawGraffiti[id] = false;
-      },
-    });
-  }
-);
+      }
+    },
+    onEnter: () => {
+      console.log(`Entered range of point ${id}`);
+    },
+    onExit: () => {
+      console.log(`Left range of point ${id}`);
+      graffiti.displayed = false;
+      drawGraffiti[id] = false;
+    },
+  });
+});
 
 async function startSpray(coords: Cfx.Vector3, text: string, size: number, font: number, hex: string) {
   const ptxDict = 'scr_recartheft';
@@ -88,19 +76,7 @@ async function startSpray(coords: Cfx.Vector3, text: string, size: number, font:
 
   await requestAssets(ptxDict);
 
-  TaskPlayAnim(
-    cache.ped,
-    'anim@amb@business@weed@weed_inspecting_lo_med_hi@',
-    'weed_spraybottle_stand_spraying_01_inspector',
-    1.0,
-    1.0,
-    -1,
-    49,
-    0,
-    false,
-    false,
-    false
-  );
+  TaskPlayAnim(cache.ped, 'anim@amb@business@weed@weed_inspecting_lo_med_hi@', 'weed_spraybottle_stand_spraying_01_inspector', 1.0, 1.0, -1, 49, 0, false, false, false);
 
   const rgbaColor = hexToRgb(hex);
   if (!rgbaColor) return;
@@ -123,23 +99,7 @@ function sprayObject() {
   const objPosition = new Cfx.Vector3(0.07, 0.03, -0.07);
   const objRotation = new Cfx.Vector3(15, 45, 10);
 
-  AttachEntityToEntity(
-    obj,
-    cache.ped,
-    GetPedBoneIndex(cache.ped, 57005),
-    objPosition.x,
-    objPosition.y,
-    objPosition.z,
-    objRotation.x,
-    objRotation.y,
-    objRotation.z,
-    true,
-    true,
-    false,
-    true,
-    0,
-    true
-  );
+  AttachEntityToEntity(obj, cache.ped, GetPedBoneIndex(cache.ped, 57005), objPosition.x, objPosition.y, objPosition.z, objRotation.x, objRotation.y, objRotation.z, true, true, false, true, 0, true);
 
   return obj;
 }
@@ -156,36 +116,15 @@ function sprayObjectCleanup(sprayObject: number) {
   ClearPedTasks(cache.ped);
 }
 
-function sprayParticles(
-  ptxDict: string,
-  ptxName: string,
-  rgbaColor: { r: number; g: number; b: number },
-  alphaValue: number
-) {
+function sprayParticles(ptxDict: string, ptxName: string, rgbaColor: { r: number; g: number; b: number }, alphaValue: number) {
   const fwdVector = GetEntityForwardVector(cache.ped);
   const plyCoords = GetEntityCoords(cache.ped, true);
   const plyHeading = GetEntityHeading(cache.ped);
-  const ptxCoords = {
-    x: plyCoords[0] + fwdVector[0] * 0.5,
-    y: plyCoords[1] + fwdVector[1] * 0.5,
-    z: plyCoords[2] - 0.5,
-  };
+  const ptxCoords = { x: plyCoords[0] + fwdVector[0] * 0.5, y: plyCoords[1] + fwdVector[1] * 0.5, z: plyCoords[2] - 0.5 };
 
   UseParticleFxAsset(ptxDict);
   SetParticleFxNonLoopedColour(rgbaColor.r / 255, rgbaColor.g / 255, rgbaColor.b / 255);
-  StartNetworkedParticleFxNonLoopedAtCoord(
-    ptxName,
-    ptxCoords.x,
-    ptxCoords.y,
-    ptxCoords.z + 1.5,
-    0,
-    0,
-    plyHeading,
-    0.5,
-    false,
-    false,
-    true
-  );
+  StartNetworkedParticleFxNonLoopedAtCoord(ptxName, ptxCoords.x, ptxCoords.y, ptxCoords.z + 1.5, 0, 0, plyHeading, 0.5, false, false, true);
 }
 
 netEvent('fivem-graffiti:client:deleteGraffitiTag', (id: number) => {
--- client/utils.ts
+++ client/utils.ts
@@ -1,3 +1,5 @@
+import { cache } from '@overextended/ox_lib/client';
+
 export function netEvent<T extends any[]>(event: string, fn: (...args: T) => void) {
   onNet(event, (...args: T) => {
     if (!source || (source as any) == '') return;
@@ -13,107 +15,53 @@ export function hexToRgb(hexCode: string) {
     return null;
   }
 
-  let rColor: number = parseInt(resultColor[1], 16);
-  let gColor: number = parseInt(resultColor[2], 16);
-  let bColor: number = parseInt(resultColor[3], 16);
+  let rColor = parseInt(resultColor[1], 16);
+  let gColor = parseInt(resultColor[2], 16);
+  let bColor = parseInt(resultColor[3], 16);
 
   return { r: rColor, g: gColor, b: bColor };
 }
 
 export function getDirectionFromRotation(rotation: { z: number; x: number }) {
-  var z: number = rotation.z * (Math.PI / 180.0);
-  var x: number = rotation.x * (Math.PI / 180.0);
-  var num: number = Math.abs(Math.cos(x));
+  var z = rotation.z * (Math.PI / 180.0);
+  var x = rotation.x * (Math.PI / 180.0);
+  var num = Math.abs(Math.cos(x));
 
-  return {
-    x: -Math.sin(z) * num,
-    y: Math.cos(z) * num,
-    z: Math.sin(x),
-  };
+  return { x: -Math.sin(z) * num, y: Math.cos(z) * num, z: Math.sin(x) };
 }
 
 export function calculateRotationFromNormal(normal: { x: number; y: number; z: number }) {
-  const length: number = Math.sqrt(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z);
-  const newX: number = normal.x / length;
-  const newY: number = normal.y / length;
-  const newZ: number = normal.z / length;
+  const length = Math.sqrt(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z);
+  const newX = normal.x / length;
+  const newY = normal.y / length;
+  const newZ = normal.z / length;
 
-  let newNormal: {
-    x: number;
-    y: number;
-    z: number;
-  } = { x: newX, y: newY, z: newZ };
+  let newNormal = { x: newX, y: newY, z: newZ };
 
-  const pitchValue: number = Math.asin(-newNormal.z);
-  const rollValue: number = Math.atan2(newNormal.y, newNormal.x);
-  const yawValue: number = Math.atan2(-newNormal.x, newNormal.y);
+  const pitchValue = Math.asin(-newNormal.z);
+  const rollValue = Math.atan2(newNormal.y, newNormal.x);
+  const yawValue = Math.atan2(-newNormal.x, newNormal.y);
 
-  const pitchDeg: number = ((pitchValue * 180) / Math.PI + 360) % 360;
-  const rollDeg: number = ((rollValue * 180) / Math.PI + 360) % 360;
-  const yawDeg: number = ((yawValue * 180) / Math.PI + 360) % 360;
+  const pitchDeg = ((pitchValue * 180) / Math.PI + 360) % 360;
+  const rollDeg = ((rollValue * 180) / Math.PI + 360) % 360;
+  const yawDeg = ((yawValue * 180) / Math.PI + 360) % 360;
 
   return { x: pitchDeg, y: rollDeg, z: yawDeg };
 }
 
-export function getRaycast(): {
-  result: number;
-  hit: any;
-  endCoords: number[];
-  surfaceNormal: number[];
-  entityHit: number;
-} {
+export function getRaycast(): { result: number; hit: any; endCoords: number[]; surfaceNormal: number[]; entityHit: number } {
   // Get start position as an array and map it to x, y, z
-  let startPositionArray: number[] = GetFinalRenderedCamCoord();
-  let startPosition: {
-    x: number;
-    y: number;
-    z: number;
-  } = {
-    x: startPositionArray[0],
-    y: startPositionArray[1],
-    z: startPositionArray[2],
-  };
+  let startPositionArray = GetFinalRenderedCamCoord();
+  let startPosition = { x: startPositionArray[0], y: startPositionArray[1], z: startPositionArray[2] };
 
   // Get camera rotation and map it to x, y, z
-  let cameraRotationArray: number[] = GetFinalRenderedCamRot(2);
-  let cameraRotation: {
-    x: number;
-    y: number;
-    z: number;
-  } = {
-    x: cameraRotationArray[0],
-    y: cameraRotationArray[1],
-    z: cameraRotationArray[2],
-  };
+  let cameraRotationArray = GetFinalRenderedCamRot(2);
+  let cameraRotation = { x: cameraRotationArray[0], y: cameraRotationArray[1], z: cameraRotationArray[2] };
 
-  let fwdVector: {
-    x: number;
-    y: number;
-    z: number;
-  } = getDirectionFromRotation(cameraRotation);
+  let fwdVector = getDirectionFromRotation(cameraRotation);
+  let frontOf = { x: startPosition.x + fwdVector.x * 10, y: startPosition.y + fwdVector.y * 10, z: startPosition.z + fwdVector.z * 10 };
 
-  let frontOf: {
-    x: number;
-    y: number;
-    z: number;
-  } = {
-    x: startPosition.x + fwdVector.x * 10,
-    y: startPosition.y + fwdVector.y * 10,
-    z: startPosition.z + fwdVector.z * 10,
-  };
-
-  let raycastTest: number = StartShapeTestLosProbe(
-    startPosition.x,
-    startPosition.y,
-    startPosition.z,
-    frontOf.x,
-    frontOf.y,
-    frontOf.z,
-    -1,
-    PlayerPedId(),
-    4
-  );
-
+  let raycastTest = StartShapeTestLosProbe(startPosition.x, startPosition.y, startPosition.z, frontOf.x, frontOf.y, frontOf.z, -1, cache.ped, 4);
   let [result, hit, endCoords, surfaceNormal, entityHit] = GetShapeTestResultIncludingMaterial(raycastTest);
 
   return { result, hit, endCoords, surfaceNormal, entityHit };
