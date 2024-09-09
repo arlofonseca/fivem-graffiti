82550d6c3a27a1ad07768705bb92d55b3c98cadc
arlofonseca <lj6976294@mail.com>
2024-09-08 21:51:56 -0400

    oops
    I was just pushing buttons I never used before. I’m so accustomed to using the command line, just wanted to see what it did lol.

--- client/main.ts
+++ client/main.ts
@@ -1,5 +1,5 @@
 import * as Cfx from '@nativewrappers/client';
-import { Point, triggerServerCallback } from '@overextended/ox_lib/client';
+import lib, { cache, Point, triggerServerCallback } from '@overextended/ox_lib/client';
 import * as config from '../config.json';
 import { netEvent, hexToRgb, getDirectionFromRotation, calculateRotationFromNormal, getRaycast } from './utils';
 
@@ -18,20 +18,9 @@ interface Graffiti {
 const createdGraffiti: Record<number, Graffiti> = {};
 const drawGraffiti: Record<number, boolean> = {};
 const points: Record<number, Point> = {};
-let playerBucket: number = 0;
+let playerBucket = 0;
 
-netEvent(
-  'fivem-graffiti:client:createGraffitiTag',
-  (
-    id: number,
-    creator_id: string,
-    coords: [number, number, number],
-    dimension: number,
-    text: string,
-    font: number,
-    size: number,
-    hex: string
-  ): void => {
+netEvent('fivem-graffiti:client:createGraffitiTag', (id: number, creator_id: string, coords: [number, number, number], dimension: number, text: string, font: number, size: number, hex: string): void => {
     const graffiti: Graffiti = {
       id: id,
       creator_id: creator_id,
@@ -47,9 +36,9 @@ netEvent(
     createdGraffiti[id] = graffiti;
     drawGraffiti[id] = false;
 
-    startSpray(coords, text, font, size, hex);
+    startSpray(graffiti.coords, text, font, size, hex);
 
-    const point = new Point({
+    points[id] = new Point({
       coords: coords,
       distance: config.graffiti_distance,
       nearby: async (): Promise<void> => {
@@ -78,149 +67,103 @@ netEvent(
         drawGraffiti[id] = false;
       },
     });
-
-    points[id] = point;
   }
 );
 
 async function startSpray(coords: Cfx.Vector3, text: string, size: number, font: number, hex: string) {
   const ptxDict = 'scr_recartheft';
   const ptxName = 'scr_wheel_burnout';
-  const sprayPos = new Cfx.Vector3(0.07, 0.03, -0.07);
-  const sprayRot = new Cfx.Vector3(15, 45, 10);
-  const sprayObject = CreateObject(GetHashKey('ng_proc_spraycan01b'), 0, 0, 0, false, false, false);
+  const obj = sprayObject();
 
-  AttachEntityToEntity(
-    sprayObject,
-    PlayerPedId(),
-    GetPedBoneIndex(PlayerPedId(), 57005), // Bone index for the right hand
-    sprayPos.x,
-    sprayPos.y,
-    sprayPos.z,
-    sprayRot.x,
-    sprayRot.y,
-    sprayRot.z,
-    true,
-    true,
-    false,
-    true,
-    0,
-    true
-  );
+  await requestAssets(ptxDict);
 
-  RequestAnimDict('anim@amb@business@weed@weed_inspecting_lo_med_hi@');
-  while (!HasAnimDictLoaded('anim@amb@business@weed@weed_inspecting_lo_med_hi@')) {
-    await new Promise((resolve: (value: unknown) => void) => setTimeout(resolve, 100));
+  TaskPlayAnim(cache.ped, 'anim@amb@business@weed@weed_inspecting_lo_med_hi@', 'weed_spraybottle_stand_spraying_01_inspector', 1.0, 1.0, -1, 49, 0, false, false, false);
 
-    RequestNamedPtfxAsset(ptxDict);
-    while (!HasNamedPtfxAssetLoaded(ptxDict)) {
-      await new Promise((resolve: (value: unknown) => void) => setTimeout(resolve, 100));
+  const rgbaColor = hexToRgb(hex);
+  if (!rgbaColor) return;
+
+  let alphaValue = 0;
+  const particleInterval = setInterval(() => {
+    if (alphaValue >= 255) {
+      clearInterval(particleInterval);
+      sprayObjectCleanup(obj);
+      return;
     }
 
-    TaskPlayAnim(
-      PlayerPedId(),
-      'anim@amb@business@weed@weed_inspecting_lo_med_hi@',
-      'weed_spraybottle_stand_spraying_01_inspector',
-      1.0,
-      1.0,
-      -1,
-      49,
-      0,
-      false,
-      false,
-      false
-    );
+    sprayParticles(ptxDict, ptxName, rgbaColor, alphaValue);
+    alphaValue++;
+  }, 200);
+}
 
-    // Convert hex color to RGBA
-    const rgbaColor = hexToRgb(hex);
-    let alphaValue = 0;
+function sprayObject() {
+  const obj = CreateObject(GetHashKey('ng_proc_spraycan01b'), 0, 0, 0, false, false, false);
+  const objPosition = new Cfx.Vector3(0.07, 0.03, -0.07);
+  const objRotation = new Cfx.Vector3(15, 45, 10);
 
-    // Start spraying
-    let particleInterval = setInterval(() => {
-      if (alphaValue === 255) {
-        clearInterval(particleInterval);
+  AttachEntityToEntity(obj, cache.ped, GetPedBoneIndex(cache.ped, 57005), objPosition.x, objPosition.y, objPosition.z, objRotation.x, objRotation.y, objRotation.z, true, true, false, true, 0, true);
 
-        if (DoesEntityExist(sprayObject)) {
-          DeleteObject(sprayObject);
-        }
+  return obj;
+}
 
-        ClearPedTasks(PlayerPedId());
+async function requestAssets(ptxDict: string) {
+  await lib.requestAnimDict('anim@amb@business@weed@weed_inspecting_lo_med_hi@');
+  await lib.requestNamedPtfxAsset(ptxDict);
+}
 
-        // todo: draw graffiti
-        return;
-      }
+function sprayObjectCleanup(sprayObject: number) {
+  if (DoesEntityExist(sprayObject)) {
+    DeleteObject(sprayObject);
+  }
+  ClearPedTasks(cache.ped);
+}
 
-      const fwdVector: number[] = GetEntityForwardVector(PlayerPedId());
-      const playerCoords: number[] = GetEntityCoords(PlayerPedId(), true);
-      const ptxCoords = {
-        x: playerCoords[0] + fwdVector[0] * 0.5,
-        y: playerCoords[1] + fwdVector[1] * 0.5,
-        z: playerCoords[2] - 0.5,
-      };
-      const playerHeading: number = GetEntityHeading(PlayerPedId());
+function sprayParticles(ptxDict: string, ptxName: string, rgbaColor: { r: number; g: number; b: number }, alphaValue: number) {
+  const fwdVector = GetEntityForwardVector(cache.ped);
+  const plyCoords = GetEntityCoords(cache.ped, true);
+  const plyHeading = GetEntityHeading(cache.ped);
+  const ptxCoords = { x: plyCoords[0] + fwdVector[0] * 0.5, y: plyCoords[1] + fwdVector[1] * 0.5, z: plyCoords[2] - 0.5 };
 
-      if (!rgbaColor) {
-        return null;
-      }
+  UseParticleFxAsset(ptxDict);
+  SetParticleFxNonLoopedColour(rgbaColor.r / 255, rgbaColor.g / 255, rgbaColor.b / 255);
+  StartNetworkedParticleFxNonLoopedAtCoord(ptxName, ptxCoords.x, ptxCoords.y, ptxCoords.z + 1.5, 0, 0, plyHeading, 0.5, false, false, true);
+}
 
-      UseParticleFxAsset(ptxDict);
-      SetParticleFxNonLoopedColour(rgbaColor.r / 255, rgbaColor.g / 255, rgbaColor.b / 255);
-      StartNetworkedParticleFxNonLoopedAtCoord(
-        ptxName,
-        ptxCoords.x,
-        ptxCoords.y,
-        ptxCoords.z + 1.5,
-        0,
-        0,
-        playerHeading,
-        0.5,
-        false,
+netEvent('fivem-graffiti:client:deleteGraffitiTag', (id: number) => {
+  if (points[id]) {
+    points[id].remove();
+    delete points[id];
+  }
+  delete createdGraffiti[id];
+  delete drawGraffiti[id];
+});
+
+on('onClientResourceStart', (resourceName: string) => {
+  if (resourceName !== 'fivem-graffiti') return;
+
+  emitNet('fivem-graffiti:server:loadGraffitiTags');
+});
+
+setInterval(async () => {
+  const dimension: number | void = await triggerServerCallback('fivem-graffiti:server:getRoutingBucket', null);
+  if (!dimension) return;
+
+  playerBucket = dimension;
+}, 1000);
+
+setInterval(async () => {
+  for (const id in drawGraffiti) {
+    if (drawGraffiti[id]) {
+      const graffiti = createdGraffiti[id];
+      Cfx.World.drawMarker(
+        Cfx.MarkerType.QuestionMark,
+        graffiti.coords,
+        Cfx.Vector3.create(0),
+        new Cfx.Vector3(0, 360, 0),
+        Cfx.Vector3.create(0.5),
+        new Cfx.Color(204, 230, 217, 188),
         false,
         true
       );
-
-      alphaValue++;
-    }, 200);
+    }
   }
-
-  netEvent('fivem-graffiti:client:deleteGraffitiTag', (id: number): void => {
-    if (points[id]) {
-      points[id].remove();
-      delete points[id];
-    }
-
-    delete createdGraffiti[id];
-    delete drawGraffiti[id];
-  });
-
-  on('onClientResourceStart', (resourceName: string): void => {
-    if (resourceName !== 'fivem-graffiti') return;
-
-    emitNet('fivem-graffiti:server:loadGraffitiTags');
-  });
-
-  setInterval(async (): Promise<void> => {
-    const bucket: number | void = await triggerServerCallback('fivem-graffiti:server:getRoutingBucket', null);
-    if (!bucket) return;
-
-    playerBucket = bucket;
-  }, 1000);
-
-  setInterval(async (): Promise<void> => {
-    for (const id in drawGraffiti) {
-      if (drawGraffiti[id]) {
-        const graffiti = createdGraffiti[id];
-        Cfx.World.drawMarker(
-          Cfx.MarkerType.QuestionMark,
-          graffiti.coords,
-          Cfx.Vector3.create(0),
-          new Cfx.Vector3(0, 360, 0),
-          Cfx.Vector3.create(0.5),
-          new Cfx.Color(204, 230, 217, 188),
-          false,
-          true
-        );
-      }
-    }
-  });
-}
+});
